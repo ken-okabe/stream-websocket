888961de57be560b641220f9e0fa1af181fcdd17
ken <kenokabe@gmail.com>
2014-03-08 00:52:35 +0900

    commit

---
+++ index.js
@@ -0,0 +1,156 @@
+/* jslint node: true */
+/* global describe, it, before, beforeEach, after, afterEach */
+
+'use strict';
+
+// WebSocket API
+// http://www.w3.org/TR/2012/CR-websockets-20120920/
+
+// Node.js v0.10.1
+// http://nodejs.org/docs/v0.10.1/api/stream.html
+var stream = require('stream');
+
+var isOpen;
+var hasBufferedData;
+
+var WebSocketStream = function(socket, options)
+{
+      var socketStream = this;
+      // From _stream_readable.  This should be Infinity but see
+      // https://github.com/joyent/node/pull/4955
+      var MAX_HWM = 0x800000;
+
+      var duplexOptions = options ||
+      {};
+
+      duplexOptions.decodeStrings = false;
+      duplexOptions.highWaterMark = MAX_HWM;
+      stream.Duplex.call(this, duplexOptions);
+      this.socket = socket;
+
+      isOpen = function(socket1)
+      {
+            return (socket1.readyState == socket.OPEN);
+      };
+      hasBufferedData = function(socket1)
+      {
+            return socket1.hasBufferedData;
+      };
+      socket.onmessage = function(event)
+      {
+            // Convert ArrayBuffers into Buffers.
+            socketStream.push(event.type == 'Binary' ? new Buffer(event.data) : event.data === '' ? null : event.data);
+      };
+      socket.onopen = function()
+      {
+            socketStream.emit('open');
+      };
+      socket.onerror = function()
+      {
+            socketStream.emit('error');
+      };
+      socket.onclose = function()
+      {
+            socketStream.emit('close');
+      };
+
+      //inherits close, error, and override finish
+      this.on('finish', function()
+      {
+            // Use empty string to represent null.
+            this.write('');
+            socket.close();
+      });
+      this.on('close', function()
+      {
+            socket.close();
+      });
+      this.on('error', function()
+      {
+            socket.close();
+      });
+};
+
+require('util')
+      .inherits(WebSocketStream, stream.Duplex);
+
+
+WebSocketStream.prototype._read = function(size)
+{
+      // Nothing to do here.
+};
+
+WebSocketStream.prototype._write = function(chunk, encoding, callback)
+{
+      var webSocketStream = this;
+      var socket = this.socket;
+
+      var send = function()
+      {
+            // Convert Buffers into ArrayBuffers
+            socket.send(chunk instanceof Buffer ? new Uint8Array(chunk) : chunk);
+
+            callback();
+      };
+
+      if (isOpen(socket))
+      {
+            send();
+      }
+      else
+      {
+            webSocketStream.on('open', function()
+            {
+                  send();
+            });
+      }
+};
+
+WebSocketStream.prototype.write = function(chunk, encoding, callback)
+{
+      var webSocketStream = this;
+      var socket = this.socket;
+      var flushed;
+      // Write the chunk and ignore Duplex's return value because we've maxed
+      // out the high water mark.
+      stream.Duplex.prototype.write.call(this, chunk, encoding, callback);
+
+      // We have to poll socket.bufferedAmount because there's no event to
+      // tell us when it's zero.  :-(
+
+      var checkBufferedAmount = function()
+      {
+            if (!hasBufferedData(socket))
+            {
+                  webSocketStream.emit('drain');
+            }
+            else
+            {
+                  setTimeout(checkBufferedAmount, 50);
+            }
+      };
+
+      if (isOpen(socket))
+      {
+            flushed = !hasBufferedData(socket);
+
+            if (!flushed)
+            {
+                  checkBufferedAmount();
+            }
+      }
+      else
+      {
+            flushed = false;
+
+            webSocketStream.on('open', function()
+            {
+                  checkBufferedAmount();
+            });
+
+      }
+
+      return flushed;
+};
+
+module.exports = WebSocketStream;
